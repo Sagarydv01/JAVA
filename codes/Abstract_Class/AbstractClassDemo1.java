5fe96c26938afa3d88bef8d5f349485c148109b0
Sagarydv01 <sagydv01@gmail.com>
2024-02-29 23:44:54 +0530

    Changed folder of Abstract Class from prev to code folder

---
+++ codes/Abstract_Class/AbstractClassDemo1.java
@@ -0,0 +1,24 @@
+package Abstract_Class;
+abstract class A {
+    abstract void A1();
+
+    void A2() {
+        System.out.println("A");
+    }
+}
+class B extends A {
+    void A1() {
+        System.out.println("From Class A - An abstract method!");
+    }
+    void B1(){
+        System.out.println("B");
+    }
+}
+class AbstractClassDemo1 {
+    public static void main(String[] args) {
+        B obj1 = new B();
+        obj1.A1();  // Abstract method of Class A
+        obj1.A2();
+        obj1.B1();
+    }
+}
---
+++ codes/Abstract_Class/AbstractClassDemo2.java
@@ -0,0 +1,35 @@
+/*  what's in this program
+ * Created 3 classes for abstract demo
+ * In which i don't created Class B as abstract or 
+ * can say does not define abstract method in Class B
+ * because of which B also becomes an Abstrct class
+ * But i don't used abstract keyword with Class B
+ */ 
+abstract class A {
+    abstract void print();
+    void displayA() {
+        System.out.println("Class A!");
+    }
+}
+class B extends A {
+    void displayB () {
+        System.out.println("Class B!");
+    }
+}
+class C extends B {
+    void print() {
+        System.out.println("Abstract method!");
+    }
+    void displayC () {
+        System.out.println("Class C!");
+    }
+}
+class AbstractClassDemo2 {
+    public static void main(String[] args) {
+        C obj = new C();
+        obj.print();
+        obj.displayA();
+        obj.displayB();
+        obj.displayC();
+    }
+}
