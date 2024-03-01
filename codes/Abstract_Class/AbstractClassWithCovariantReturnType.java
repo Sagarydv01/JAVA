905975d93a86940ab8a130d4e66655ebad02dc18
Sagarydv01 <sagydv01@gmail.com>
2024-03-01 09:59:31 +0530

    Created AbstractClassWithCovariantReturnType.java and screenshot of it with output

---
+++ codes/Abstract_Class/AbstractClassWithCovariantReturnType.java
@@ -0,0 +1,29 @@
+// Abstract Class with Covariant return type
+abstract class A {
+	abstact A print();
+	void displayA() {
+		System.out.println("Class A");
+	}
+}
+
+class B extends A {
+	B print() {
+		System.out.println("Abstract Class A method");
+		return this();
+	}
+}
+
+class C extends C {
+	C print() {
+		System.out.println("Class C");
+		return this;
+	}
+}
+
+class AbstractClassWithCovariantReturnType {
+	public static void main(String[] args) {
+		C obj = new C();
+		obj.print();
+		obj.displayA();
+	}
+}
