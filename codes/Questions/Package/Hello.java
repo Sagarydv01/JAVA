07c87bab0ec5de5cb90697274d41466cba231123
Sagarydv01 <sagydv01@gmail.com>
2024-03-15 18:15:35 +0530

    Added Folder Package to practice

---
+++ codes/Questions/Package/Hello.java
@@ -0,0 +1,10 @@
+package Questions.Package;
+
+public class Hello {
+    void display() {
+        System.out.println("Radhey Radhey!");
+    }
+    /* public static void main(String[] args) {
+        System.out.println("Hello!");
+    } */
+}
---
+++ codes/Questions/Package/Test.java
@@ -0,0 +1,9 @@
+package Questions.Package;
+
+public class Test extends Hello {
+    public static void main(String[] args) {
+        Hello obj = new Hello();
+        obj.display();
+        System.out.println("Sagar Yadav!");
+    }
+}
