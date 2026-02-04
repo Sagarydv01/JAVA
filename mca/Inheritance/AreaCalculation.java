760c939a6ee3abb8384b0c16c1cb23beadfb6e6a
Sagarydv01 <sagydv01@gmail.com>
2026-02-04 22:06:24 +0530

    Added a simple inheritance program -  AreaCalculation

---
+++ mca/Inheritance/AreaCalculation.java
@@ -0,0 +1,35 @@
+package Inheritance;
+import java.util.Scanner;
+
+class Circle {
+    double area(double radius) {
+        return Math.PI * radius * radius;
+    }
+}
+
+class Rectangle extends Circle {
+    double area(double length, double breadth) {
+        return length * breadth;
+    }
+}
+
+public class AreaCalculation {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        Rectangle rectangle = new Rectangle();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter radius of circle: ");
+        double radius = sc.nextDouble();
+        System.out.print("Enter length and breadth of rectangle: ");
+        double length = sc.nextDouble();
+        double breadth = sc.nextDouble();
+        sc.close();
+
+        double circleArea = circle.area(radius);
+        double rectangleArea = rectangle.area(length, breadth);
+
+        System.out.printf("Area of Circle: %.2f%n", circleArea);
+        System.out.printf("Area of Rectangle: %.2f%n", rectangleArea);
+    }
+}
