11b2aa5de596ee37bb92401655aaf1cb4a36e66f
Sagarydv01 <sagydv01@gmail.com>
2026-02-19 12:09:44 +0530

    Package

---
+++ mca/myPackage/TestPackage.java
@@ -0,0 +1,10 @@
+// package myPackage;
+import myPackage.Hello;
+
+public class TestPackage extends Hello {
+    public static void main(String[] args) {
+        Hello obj = new Hello();
+
+        obj.display("sagar");
+    }
+}
