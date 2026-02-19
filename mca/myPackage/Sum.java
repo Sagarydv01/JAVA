283a900534f22969e8ba574ca0df6a1e0ebef15a
Sagarydv01 <sagydv01@gmail.com>
2026-02-19 12:26:23 +0530

    package 2

---
+++ mca/myPackage/Sum.java
@@ -0,0 +1,7 @@
+package myPackage;
+
+public class Sum {
+    public void sum(int a, int b) {
+        System.out.println(a + b);
+    }
+}
--- mca/myPackage/TestPackage.java
+++ mca/myPackage/TestPackage.java
@@ -1,10 +1,14 @@
 // package myPackage;
 import myPackage.Hello;
+import myPackage.Sum;
 
-public class TestPackage extends Hello {
+
+public class TestPackage {
     public static void main(String[] args) {
         Hello obj = new Hello();
+        Sum sm1 = new Sum();
 
         obj.display("sagar");
+        sm1.sum(111, 322);
     }
 }
