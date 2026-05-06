2059a41ad80bb41abc05faa32f2b7589b19c4fa0
Sagar Yadav <sagydv01@gmail.com>
2026-05-06 16:58:05 +0530

    add: mca/Practical File/tempCodeRunnerFile.java

---
+++ mca/Practical File/tempCodeRunnerFile.java
@@ -0,0 +1,7 @@
+// Add action listeners
+        // newItem.addActionListener(e -> JOptionPane.showMessageDialog(frame, "New selected"));
+        // openItem.addActionListener(e -> JOptionPane.showMessageDialog(frame, "Open selected"));
+        // exitItem.addActionListener(e -> System.exit(0));
+        // cutItem.addActionListener(e -> JOptionPane.showMessageDialog(frame, "Cut selected"));
+        // copyItem.addActionListener(e -> JOptionPane.showMessageDialog(frame, "Copy selected"));
+        // pasteItem.addActionListener(e -> JOptionPane.showMessageDialog(frame, "Paste selected"));
